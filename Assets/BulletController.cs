6e3f7bb3cb3732597e36a24ff15be4c2f273d9ed
Amber L <AmberL@AmberdeMacBook-Pro.local>
2019-03-10 17:01:57 -0400

    not working gun

---
+++ Assets/BulletController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO: use a bullet pool later
+public class BulletController : MonoBehaviour
+{
+    public float Speed = 3.0f;
+    public float LongestDistance = 9.0f;
+    private Rigidbody rb;
+    private Vector3 originPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        originPos = transform.position;
+        //Vector3 direction = new Vector3(-1, 0, 0);
+        Vector3 direction = transform.rotation * -transform.right;
+        print(direction);
+        //Vector3 direction = transform.forward;
+        rb.velocity = direction * Speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs((transform.position - originPos).magnitude) > LongestDistance)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //public void Shoot()
+    //{
+    //    Vector3 direction = new Vector3(-1, 0, 0);
+    //    rb.velocity = direction * Speed;
+    //}
+}
---
+++ Assets/WeaponController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            GameObject bullet = (GameObject)Instantiate(bulletPrefab, transform.position - new Vector3(2, 0 ,0), transform.rotation);
+            bullet.transform.parent = gameObject.transform;
+            //bullet.GetComponent<BulletController>().Shoot();
+        }
+    }
+
+    //public static BulletController Create()
+    //{
+    //    GameObject bullet = (GameObject)Instantiate(bulletPrefab, transform.position - new Vector3(2, 0, 0), Quaternion.identity);
+    //    BulletController yourObject = bullet.GetComponent<BulletController>();
+    //    //do additional initialization steps here
+
+    //    return yourObject;
+    //}
+}
