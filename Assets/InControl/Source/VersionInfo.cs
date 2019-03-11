6baa31bf0cbeb533deb3ef7ddf391d7b755a1ab9
zanhua <zanhua@umich.edu>
2019-03-11 03:18:33 -0400

    vinfo

--- Assets/InControl/Source/VersionInfo.cs
+++ Assets/InControl/Source/VersionInfo.cs
@@ -29,7 +29,7 @@ namespace InControl
                 Major = 1,
                 Minor = 4,
                 Patch = 4,
-                Build = 4065
+                Build = 4067
             };
         }
 
