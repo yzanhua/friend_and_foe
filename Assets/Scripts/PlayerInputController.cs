c4869a847f043356c198feb5247b540f6a85ec90
涂凯巍 <kaiweitu@umich.edu>
2019-03-10 20:06:45 -0400

    Add player prefab

--- Assets/Scripts/PlayerInputController.cs
+++ Assets/Scripts/PlayerInputController.cs
@@ -10,6 +10,14 @@ public class PlayerInputController : MonoBehaviour
     [Range(0f, 10f)]
     public float speed = 2f;
 
+
+    private Animator an;
+
+    void Start()
+    {
+        an = GetComponent<Animator>();
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -17,7 +25,26 @@ public class PlayerInputController : MonoBehaviour
 
         if (inputDevice != null)
         {
-            transform.position += new Vector3(inputDevice.DPadX, inputDevice.DPadY, 0) * Time.deltaTime * speed;   
+            an.speed = 1.0f;
+
+            if (System.Math.Abs(inputDevice.DPadX) > 0.1f)
+            {
+                an.SetFloat("horizontal", inputDevice.DPadX);
+                an.SetFloat("vertical", 0f);
+                transform.position += new Vector3(inputDevice.DPadX, 0, 0) * Time.deltaTime * speed;
+            }
+            else if (System.Math.Abs(inputDevice.DPadY) > 0.1f)
+            {
+                an.SetFloat("vertical", inputDevice.DPadY);
+                an.SetFloat("horizontal", 0f);
+                transform.position += new Vector3(0, inputDevice.DPadY, 0) * Time.deltaTime * speed;
+            }
+            else
+            {
+                an.speed = 0.0f;
+            }
         }
+
     }
+
 }
