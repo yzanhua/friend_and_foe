288d335be1ee8385813f82bc788a2687f70403ec
涂凯巍 <kaiweitu@umich.edu>
2019-03-11 06:48:07 -0400

    Main Scene update

--- Assets/InControl/Source/VersionInfo.cs
+++ Assets/InControl/Source/VersionInfo.cs
@@ -29,7 +29,7 @@ namespace InControl
                 Major = 1,
                 Minor = 4,
                 Patch = 4,
-                Build = 4178
+                Build = 4179
             };
         }
 
