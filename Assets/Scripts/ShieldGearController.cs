65bc70a8dc921c0141390b53678c5018af7eb212
Amber L <AmberL@AmberdeMacBook-Pro.local>
2019-03-14 20:03:37 -0400

    untrack versioninfo

---
+++ Assets/Scripts/ShieldGearController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldGearController : MonoBehaviour
+{
+
+    GameObject _currPlayer;
+    GameObject _shield;
+    GameObject _submarine;
+    float _initGravityScale;
+    float _lastFireDelta;
+    bool hasPlayer = false;
+
+    public float ShieldTime = 6f;
+    public float PlayerCD = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _shield = transform.parent.Find("BubbleShield").gameObject;
+        _submarine = transform.parent.parent.gameObject;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            _currPlayer = collision.gameObject;
+            _currPlayer.transform.position = transform.position;
+            _initGravityScale = _currPlayer.GetComponent<Rigidbody2D>().gravityScale;
+            _currPlayer.GetComponent<Rigidbody2D>().gravityScale = 0;
+            hasPlayer = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            _currPlayer.GetComponent<Rigidbody2D>().gravityScale = _initGravityScale;
+            hasPlayer = false;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasPlayer)
+            return;
+
+        PlayerInputController inputController = _currPlayer.GetComponent<PlayerInputController>();
+        if (inputController.inputDevice.Action2)
+        {
+            bool success = _shield.GetComponent<BubbleShieldController>().Defense();
+            if (success)
+            {
+                _currPlayer.GetComponent<PlayerMovementController>().movementEnable = false;
+                StartCoroutine(WaitTillBreak());
+                StartCoroutine(WaitPlayerCD());
+            }
+        }
+    }
+
+    IEnumerator WaitTillBreak()
+    {
+        yield return new WaitForSeconds(ShieldTime);
+        _shield.GetComponent<BubbleShieldController>().BreakShield();
+    }
+
+    IEnumerator WaitPlayerCD()
+    {
+        yield return new WaitForSeconds(PlayerCD);
+        _currPlayer.GetComponent<PlayerMovementController>().movementEnable = true;
+    }
+}
---
+++ Assets/Scripts/TimerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    public Text timer;
+    [Range(0, 9)]
+    public int start_time = 5;
+
+    private int _min;
+    private int _second;
+
+    void Start()
+    {
+        _min = start_time;
+        _second = 0;
+        StartCoroutine(countDown());
+    }
+
+    IEnumerator countDown()
+    {
+        while(true) {
+            if (_second == 0)
+            {
+                _min--;
+                _second = 59;
+            }
+            else
+            {
+                _second--;
+            }
+
+            if (_min == 0 && _second == 0)
+            {
+                // Game end call game controller
+                GameController.instance.GameEnd();
+                break;
+            }
+
+            if (_second < 10)
+            {
+                timer.text = "0" + _min + ":0" + _second;
+            }
+            else
+            {
+                timer.text = "0" + _min + ":" + _second;
+            }
+
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
