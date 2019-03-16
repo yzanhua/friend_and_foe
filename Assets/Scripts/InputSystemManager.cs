838cddc1aa69698c436de5eb8bf5fce04930a571
涂凯巍 <kaiweitu@umich.edu>
2019-03-15 23:49:50 -0400

    Add input system support

---
+++ Assets/Scripts/InputSystemManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Experimental.Input;
+
+public class InputSystemManager : MonoBehaviour
+{
+    static public InputSystemManager instance;
+
+
+    private List<Gamepad> _gamepad_list;
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    void Start()
+    {
+        _gamepad_list = new List<Gamepad>(Gamepad.all);
+    }
+
+
+    static public Gamepad GetGamePad(int playerID) 
+    {
+        Gamepad gp = playerID >= instance._gamepad_list.Count ? null : instance._gamepad_list[playerID];
+        return gp;
+    }
+}
---
+++ Assets/Scripts/InputSystemSample.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Experimental.Input;
+
+
+public class InputSystemTest : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Gamepad active_gamepad = InputSystemManager.GetGamePad(0);
+
+        float horizontal_val = active_gamepad.leftStick.x.ReadValue();
+        float vertical_val = active_gamepad.leftStick.y.ReadValue();
+
+        bool a_pressed_this_frame = active_gamepad.aButton.wasPressedThisFrame;
+        bool b_pressed_this_frame = active_gamepad.bButton.wasPressedThisFrame;
+
+        
+        if (a_pressed_this_frame) 
+        {
+            Debug.Log("A pressed");
+            Debug.Log(horizontal_val);
+            Debug.Log(vertical_val);
+        } else if (b_pressed_this_frame)
+        {
+            Debug.Log("B Pressed");
+        }
+
+    }
+}
