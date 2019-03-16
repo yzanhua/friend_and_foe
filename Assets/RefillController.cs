8b3c66ef6b4234050d840fa8b5f737a0675065f0
涂凯巍 <kaiweitu@umich.edu>
2019-03-16 01:38:09 -0400

    Add progress bar to the refill station

--- Assets/RefillController.cs
+++ Assets/RefillController.cs
@@ -6,29 +6,43 @@ public class RefillController : MonoBehaviour
 {
     public float refillTime = 2.0f;
     public GameObject weapon;
+    public GameObject progress_bar;
 
     float curFilledTime = 0;
     // whether key was pressed in this collision period
     bool keyDown = false;
     bool playerTrigger = false;
+    bool bulletEmpty = true;
+
     PlayerInputController playerInput;
 
+    public void BulletNeedRefill()
+    {
+        bulletEmpty = true;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        progress_bar.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!bulletEmpty)
+            return;
         if (playerTrigger)
         {
             if (!keyDown && playerInput.inputDevice.Action2)
                 keyDown = true;
             if (keyDown)
             {
+                if (curFilledTime == 0)
+                {
+                    progress_bar.SetActive(true);
+                }
                 curFilledTime += Time.deltaTime;
+                progress_bar.GetComponent<HealthBar>().SetSize((float)curFilledTime / (float)refillTime);
                 print(curFilledTime);
             }
         }
@@ -38,6 +52,8 @@ public class RefillController : MonoBehaviour
             curFilledTime = 0;
             keyDown = false;
             weapon.GetComponent<WeaponController>().fillBullets();
+            progress_bar.SetActive(false);
+            bulletEmpty = false;
 
         }
     }
