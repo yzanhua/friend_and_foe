e6d0219b1eb3a5e8f99e4ac0b5917c98ba520d38
zanhua <zanhua@umich.edu>
2019-03-17 19:01:26 -0400

    Ladder movement improved

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -11,91 +11,118 @@ public class PlayerMovementController : MonoBehaviour
     public int playerID;
     public bool movementEnable = true;
 
-    private int _ladderLayer;
+    private int ladderLayer;
 
-    private Rigidbody2D _rb2d;
- 
-    private float _initGravityScale;
-
-    private Vector2 _target;
-
-    private Animator _an;
-
+    private bool onLadder = false;
+    private bool climbingLadder = false;
+    private float initGravityScale;
+    private Rigidbody2D rb2d;
+    private Vector2 target;
+    private Animator an;
     private Rigidbody2D submarine_rb;
 
     void Start()
     {
-        _rb2d = GetComponent<Rigidbody2D>();
-        _ladderLayer = LayerMask.NameToLayer("Ladder");
-        _initGravityScale = _rb2d.gravityScale;
-        _an = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+        ladderLayer = LayerMask.NameToLayer("Ladder");
+        initGravityScale = rb2d.gravityScale;
+        an = GetComponent<Animator>();
         submarine_rb = transform.parent.GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    private void Update()
     {
-        if ( !movementEnable || !Global.instance.AllPlayersMovementEnable)
+        if (!movementEnable || !Global.instance.AllPlayersMovementEnable)
         {
-            _an.speed = 0f;
-            _rb2d.velocity = submarine_rb.velocity;
+            an.speed = 0f;
+            rb2d.velocity = submarine_rb.velocity;
             return;
         }
-        
-        //movement for character and for climbing ladder
+
         float verticalInput = InputSystemManager.GetLeftSVertical(playerID);
         float horizontalInput = InputSystemManager.GetLeftSHorizontal(playerID);
-        //Debug.Log(verticalInput);
-        //Debug.Log(horizontalInput);
-        RaycastHit2D hit_climb;
-        RaycastHit2D hit_down;
-        hit_climb = Physics2D.Raycast(transform.position, Vector2.up, 1.0f, 1 << _ladderLayer);
-        hit_down = Physics2D.Raycast(transform.position, Vector2.down, 1.0f, 1 << _ladderLayer);
 
-        if (Mathf.Abs(verticalInput) > Mathf.Abs(horizontalInput))
-        {
-            horizontalInput = 0f;
-        }
-        else
-        {
+        if (!onLadder && verticalInput > 0f)
             verticalInput = 0f;
-        }
-        _an.speed = 1f;
-        _an.SetFloat("vertical", verticalInput);
-        _an.SetFloat("horizontal", horizontalInput);
+        if (climbingLadder)
+            horizontalInput = 0f;
 
-        if (verticalInput > 0 && hit_climb)
-        {
-            if (hit_climb.collider.gameObject.CompareTag("Ladder") && Mathf.Abs(hit_climb.collider.transform.position.x - transform.position.x) < 0.05f)
-            {
-                transform.position = new Vector3(hit_climb.collider.transform.position.x, transform.position.y, 0);
-            }
+        an.speed = 1f;
+        an.SetFloat("vertical", verticalInput);
+        an.SetFloat("horizontal", horizontalInput);
 
-            _rb2d.velocity = speed * new Vector2(horizontalInput, verticalInput);
-        }
-        else if (verticalInput < 0 && hit_down)
-        {
-            if (hit_down.collider.gameObject.CompareTag("Ladder") && Mathf.Abs(hit_down.collider.transform.position.x - transform.position.x) < 0.05f)
-            {
-                transform.position = new Vector3(hit_down.collider.transform.position.x, transform.position.y, 0);
-            }
-            _rb2d.velocity = speed * new Vector2(horizontalInput, verticalInput);
-        }
-        else 
-        {
-            _rb2d.velocity = speed * new Vector2(horizontalInput, 0);
-            if (_rb2d.velocity == Vector2.zero)
-            {
-                _an.speed = 0f;
-            }
-        }
-        _rb2d.velocity += submarine_rb.velocity;
+        rb2d.velocity = speed * new Vector2(horizontalInput, verticalInput);
+        rb2d.velocity += submarine_rb.velocity;
     }
+
+    //void Update()
+    //{
+    //    if ( !movementEnable || !Global.instance.AllPlayersMovementEnable)
+    //    {
+    //        _an.speed = 0f;
+    //        _rb2d.velocity = submarine_rb.velocity;
+    //        return;
+    //    }
+
+    //    //movement for character and for climbing ladder
+    //    float verticalInput = InputSystemManager.GetLeftSVertical(playerID);
+    //    float horizontalInput = InputSystemManager.GetLeftSHorizontal(playerID);
+
+    //    RaycastHit2D hit_climb;
+    //    RaycastHit2D hit_down;
+    //    hit_climb = Physics2D.Raycast(transform.position, Vector2.up, 1.0f, 1 << _ladderLayer);
+    //    hit_down = Physics2D.Raycast(transform.position, Vector2.down, 1.0f, 1 << _ladderLayer);
+
+    //    if (Mathf.Abs(verticalInput) > Mathf.Abs(horizontalInput))
+    //    {
+    //        horizontalInput = 0f;
+    //    }
+    //    else
+    //    {
+    //        verticalInput = 0f;
+    //    }
+    //    _an.speed = 1f;
+    //    _an.SetFloat("vertical", verticalInput);
+    //    _an.SetFloat("horizontal", horizontalInput);
+
+    //    if (verticalInput > 0 && hit_climb)
+    //    {
+    //        if (hit_climb.collider.gameObject.CompareTag("Ladder") && Mathf.Abs(hit_climb.collider.transform.position.x - transform.position.x) < 0.05f)
+    //        {
+    //            transform.position = new Vector3(hit_climb.collider.transform.position.x, transform.position.y, 0);
+    //        }
+
+    //        _rb2d.velocity = speed * new Vector2(horizontalInput, verticalInput);
+    //    }
+    //    else if (verticalInput < 0 && hit_down)
+    //    {
+    //        if (hit_down.collider.gameObject.CompareTag("Ladder") && Mathf.Abs(hit_down.collider.transform.position.x - transform.position.x) < 0.05f)
+    //        {
+    //            transform.position = new Vector3(hit_down.collider.transform.position.x, transform.position.y, 0);
+    //        }
+    //        _rb2d.velocity = speed * new Vector2(horizontalInput, verticalInput);
+    //    }
+    //    else 
+    //    {
+    //        _rb2d.velocity = speed * new Vector2(horizontalInput, 0);
+    //        if (_rb2d.velocity == Vector2.zero)
+    //        {
+    //            _an.speed = 0f;
+    //        }
+    //    }
+    //    _rb2d.velocity += submarine_rb.velocity;
+    //}
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Ladder"))
         {
-            _rb2d.gravityScale = 0;
+            rb2d.gravityScale = 0;
+            onLadder = true;
+        }
+        if (collision.gameObject.name == "TestCollider")
+        {
+            climbingLadder = true;
         }
     }
 
@@ -104,7 +131,12 @@ public class PlayerMovementController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ladder"))
         {
-            _rb2d.gravityScale = _initGravityScale;
+            rb2d.gravityScale = initGravityScale;
+            onLadder = false;
+        }
+        if (collision.gameObject.name == "TestCollider")
+        {
+            climbingLadder = false;
         }
     }
 }
