7b5b9b45ca26c1b36f3ea60c325679d63ed118ee
zanhua <zanhua@umich.edu>
2019-03-20 16:10:10 -0400

    player movement fixed

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -14,73 +14,80 @@ public class PlayerMovementController : MonoBehaviour
     public float DizzyTime = 1.5f;
     
     public bool movementEnable = true;
-    //public bool SeatedOnGear = false;
 
     public Animator an;
-    //public Rigidbody2D submarine_rb;
 
-    private int ladderLayer;
+    public float knockBackDistance = 300f;
 
     private bool onLadder = false;
     private bool climbingLadder = false;
+    private GameObject ladder;
     private float initGravityScale;
     private Rigidbody2D rb2d;
     private Vector2 target;
-    
-
     private bool dizzy = false;
-    private Vector2 self_speed;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        ladderLayer = LayerMask.NameToLayer("Ladder");
         initGravityScale = rb2d.gravityScale;
     }
 
     private void Update()
     {
         an.speed = 0f;
-
-        if (dizzy)
-        {
-            rb2d.velocity += self_speed;
-            return;
-        }
-
         if (!movementEnable || !Global.instance.AllPlayersMovementEnable)
             return;
 
         float verticalInput = InputSystemManager.GetLeftSVertical(playerID);
         float horizontalInput = InputSystemManager.GetLeftSHorizontal(playerID);
 
-        if (!onLadder && verticalInput > 0f)
+        if (!onLadder)
+        {
             verticalInput = 0f;
-        if (climbingLadder)
+            
+        }
+        else if (climbingLadder)
+        {
             horizontalInput = 0f;
+        }
+        if (Mathf.Abs(verticalInput) > Mathf.Abs(horizontalInput)) // horizontalInput = 0f;
+        {
+            Vector3 temp = new Vector3(0f, verticalInput, 0f);
+            temp = temp.normalized * Time.deltaTime * 2.5f;
+            temp += transform.position;
+            transform.position = new Vector3(ladder.transform.position.x, temp.y, temp.z);
+        }
+        else // verticalInput = 0f
+        {
+            Vector2 temp = new Vector2(horizontalInput, 0f);
+            temp = temp.normalized * speed;
+            rb2d.AddForce(speed * temp * rb2d.mass);
+        }
 
-        if (Mathf.Abs(verticalInput) < Mathf.Abs(horizontalInput))
-            verticalInput = 0f;
-        else
-            horizontalInput = 0f;
+        
 
+        
+
+
+
+        // set animation
         if (Mathf.Abs(verticalInput)+ Mathf.Abs(horizontalInput) > 0f)
             an.speed = 1f;
-
         an.SetFloat("vertical", verticalInput);
         an.SetFloat("horizontal", horizontalInput);
 
-        Vector2 temp = new Vector2(horizontalInput, verticalInput);
-        temp = temp.normalized * Time.deltaTime * speed;
-        rb2d.AddForce(speed * temp * rb2d.mass);
+       
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Ladder"))
         {
-            rb2d.gravityScale = 0;
+            rb2d.gravityScale = 0f;
+            rb2d.velocity = new Vector2(rb2d.velocity.x, 0f);
             onLadder = true;
+            ladder = collision.gameObject;
         }
         if (collision.gameObject.CompareTag("LadderForbidHorizontal"))
         {
@@ -108,21 +115,23 @@ public class PlayerMovementController : MonoBehaviour
         if (!other.CompareTag("Player"))
             return;
 
-        Vector3 forceDirection = (transform.position - other.transform.position).normalized;
+        Vector3 forceDirection = (other.transform.position - transform.position);
         if (climbingLadder)
             forceDirection.x = 0f;
         else
             forceDirection.y = 0f;
-        dizzy = true;
-        self_speed = forceDirection * 3;
+
+        forceDirection = forceDirection.normalized;
+        movementEnable = false;
+        rb2d.AddForce(-forceDirection* knockBackDistance);
         StartCoroutine(KnockBackEffect());
     }
 
     IEnumerator KnockBackEffect()
     {
         yield return new WaitForSeconds(KnockBackTime);
-        self_speed = Vector2.zero;
+        rb2d.velocity = Vector2.zero;
         yield return new WaitForSeconds(DizzyTime);
-        dizzy = false;
+        movementEnable = true;
     }
 }
