d7a7f06adc39227cc32847fcede6264d2f5181db
zanhua <zanhua@umich.edu>
2019-04-07 02:12:33 -0400

    fix bug in BombCreate

--- Assets/Scripts/BombCreate.cs
+++ Assets/Scripts/BombCreate.cs
@@ -9,12 +9,15 @@ public class BombCreate : MonoBehaviour
     public GameObject Bomb_prefab;
     public GameObject targetSign_prefab;
     public GameObject submarine_proxy;
-    // public int submarine_id = 1;
+
+    private float radius;
 
     private void Start()
     {
         Random.InitState((int)System.DateTime.Now.Ticks);
         //init_distance = Vector3.Distance(transform.position, transform.parent.position);
+        radius = submarine_proxy.GetComponent<CircleCollider2D>().radius * submarine_proxy.transform.localScale.x;
+        radius *= submarine_proxy.transform.parent.localScale.x;
         StartCoroutine(CreateBomb());
     }
 
@@ -26,17 +29,19 @@ public class BombCreate : MonoBehaviour
         {
             if (Global.instance.isGameEnd)
                 break;
-            Transform target_transform = transform;
-            target_transform.position = submarine_proxy.transform.position + new Vector3(2.44f, 1.15f);
-            target_transform.parent = null;
-            target_transform.RotateAround(submarine_proxy.transform.position, Vector3.forward, Random.Range(0f, 365f));
-            GameObject targetSign = Instantiate(targetSign_prefab, target_transform);
+
+            GameObject targetSign = Instantiate(targetSign_prefab);
+            targetSign.transform.position = submarine_proxy.transform.position + new Vector3(radius, 0f, 0f) * 1.2f;
+            targetSign.transform.parent = null;
+            targetSign.transform.RotateAround(submarine_proxy.transform.position, Vector3.forward, Random.Range(0f, 365f));
+
             // SoundManager.instance.PlaySound("bomb_bell");
-            yield return new WaitForSeconds(2.5f);
+            yield return new WaitForSeconds(3.5f);
             Destroy(targetSign, 0.2f);
             if (Global.instance.isGameEnd)
                 break;
-            GameObject new_bomb =  Instantiate(Bomb_prefab, target_transform);
+
+            GameObject new_bomb =  Instantiate(Bomb_prefab, targetSign.transform.position, Quaternion.identity);
             yield return new WaitForSeconds(Random.Range(MinCreateInterval, MaxCreateInterval));
         }
     }
