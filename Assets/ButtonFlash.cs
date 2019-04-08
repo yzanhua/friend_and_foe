6f46e99c3611fe6784ebbecfbfcd4744f0ffeff7
mengqiuteng <mengqiu@umich.edu>
2019-04-08 07:18:43 -0400

    Add button hint.

---
+++ Assets/ButtonFlash.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonFlash : MonoBehaviour
+{
+    //private bool
+
+    // Update is called once per frame
+
+
+    IEnumerator Flash()
+    {
+        while (true)
+        {
+            for (int i = 0; i < transform.childCount; ++i)
+            {
+                transform.GetChild(i).GetComponent<SpriteRenderer>().enabled = true;
+            }
+            yield return new WaitForSeconds(0.3f);
+            for (int i = 0; i < transform.childCount; ++i)
+            {
+                transform.GetChild(i).GetComponent<SpriteRenderer>().enabled = false;
+            }
+            yield return new WaitForSeconds(0.3f);
+        }
+    }
+}
